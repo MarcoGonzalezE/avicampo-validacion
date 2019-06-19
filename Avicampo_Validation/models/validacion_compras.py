119601dd8e50e19d2f4db038073d6b193bb45e7a
Marco Antonio Gonzalez <34143837+MarcoGonzalezE@users.noreply.github.com>
2019-06-19 12:49:43 -0500

    Relación Granjas / Encargados

--- Avicampo_Validation/models/validacion_compras.py
+++ Avicampo_Validation/models/validacion_compras.py
@@ -4,10 +4,9 @@ from datetime import datetime
 from odoo.exceptions import UserError, ValidationError
 
 class validation_purchase(models.Model): 
-    _inherit = "purchase.order" 
-    _name ="button_compra"
+    _inherit = "purchase.order"
     
-
     def enviar_granja_compras(self):
     	raise ValidationError('SEGUIMOS TRABAJANDO')
+		
  
--- Avicampo_Validation/models/validacion_transferencias.py
+++ Avicampo_Validation/models/validacion_transferencias.py
@@ -3,11 +3,32 @@ from odoo import api, fields, models
 from datetime import datetime
 from odoo.exceptions import UserError, ValidationError
 
+
+class granjasencargado(models.Model):
+	_inherit = "stock.warehouse"	
+	_name = "granja.encargado"
+
+	granja = fields.Many2one('stock.warehouse', string="Granja")
+	granja_abr = fields.Char(required=True, string = "Abreviatura")
+	encargado = fields.Many2many('res.users', string="Encargado")
+
+	@api.one
+	@api.depends('granja'):
+	def _compute_granja_abr(self):
+		self.granja_abr = self.stock.warehouse.code
+
+
+
 class validation_stock_picking(models.Model): 
-    _inherit = "stock.picking" 
-    _name ="button_granja"
+    _inherit = "stock.picking"
+
+
+    aprobado_url = fields.Char()
+    rechazado_url = fields.Char()
 
 
     def enviar_granja(self):
     	raise ValidationError('SEGUIMOS TRABAJANDO')
+
+
  
